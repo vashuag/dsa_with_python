b52f80546680baa097f83636323f345c48045b8e
Vashu Agarwal <83650895+vashuag@users.noreply.github.com>
2025-05-20 01:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.60%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,17 +1,46 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        i = len(nums) - 1
-        while i > 0 and nums[i-1] >= nums[i]:
-            i -= 1
-        
-        if i == 0:
+
+        size = len(nums)-1
+
+        while size>0 and nums [size-1]>=nums[size]:
+            size-=1
+        if  size ==0:
             nums.reverse()
             return
-        
-        j = len(nums) - 1
-        while j >= i and nums[j] <= nums[i-1]:
-            j -= 1
-        
-        nums[i-1], nums[j] = nums[j], nums[i-1]
+        nsize = len(nums)-1
 
-        nums[i:] = reversed(nums[i:])
+        while nsize>=size and nums[nsize] <=nums[size-1]:
+            nsize-=1
+        nums[size-1],nums[nsize] = nums[nsize],nums[size-1]
+
+        nums[size:] = reversed(nums[size:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # i = len(nums) - 1
+        # while i > 0 and nums[i-1] >= nums[i]:
+        #     i -= 1
+        
+        # if i == 0:
+        #     nums.reverse()
+        #     return
+        
+        # j = len(nums) - 1
+        # while j >= i and nums[j] <= nums[i-1]:
+        #     j -= 1
+        
+        # nums[i-1], nums[j] = nums[j], nums[i-1]
+
+        # nums[i:] = reversed(nums[i:])
